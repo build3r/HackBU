f850f4420ba368ce7f31e693a34de5e06d721fa3
Mark Philip <philip.mark123@gmail.com>
2017-02-26 08:07:53 -0500

    Updated facebook url. Added way to retrieve likes and category

--- api_test.py
+++ api_test.py
@@ -28,8 +28,8 @@ class Api(object):
 
 if __name__ == '__main__':
     # ebay
-    url = 'http://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=RobinLi-HackBU-SBX-16c385072-25a053d6&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=Dell%20Laptops&itemFilter.paramName=Currency&itemFilter.paramValue=USD&itemFilter.value=true&paginationInput.entriesPerPage=3'
+    #url = 'http://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=RobinLi-HackBU-SBX-16c385072-25a053d6&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=Dell%20Laptops&itemFilter.paramName=Currency&itemFilter.paramValue=USD&itemFilter.value=true&paginationInput.entriesPerPage=3'
     # facebook
-    #url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Babout%2Ccategory%2Cname%2Cdisplay_subtext%2Cgeneral_info%2Cgenre%2Ccompany_overview%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAN7gkHYqNO3BnEXoKk2nZCFOHNZC7crvAUWzmU6zZA7DdSZABm2nvz8ZAZAMPPo3ZC42ESGyQxhq2QFzGiYLA1smfSar2golM7bZAUHuABY3t80MQjSqC20xdexoJBAjOW3NswkfoVzFOe2EokDXI1Fh48T9F90AnZCZB9adaB3d0UxJSjD1CZCI6cZD'
+    url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Babout%2Ccategory%2Cname%2Cdisplay_subtext%2Cgeneral_info%2Cgenre%2Ccompany_overview%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAFwfDpZAlNNmaZBv1v40EV8ZBbGhvKQEFzbZCIfZCqMz0E0MY3OYNdkV7iBZBNDTwABZAFdxVB4f77DvdbSOxZBLMvxaN9CYThrEpXQwJQf3YwRY54egDDclgRp7ZBaLtrIZAzqkwSZALYWaSZAd4OanliwuHIMsRZCZBTiDzbXjgmy7i6lsuzW89u2wcZD'
     t = Api(url)
     t.get_response()
--- backend/backend.py
+++ backend/backend.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 ask = Ask(app, '/')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-facebook_url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Babout%2Ccategory%2Cname%2Cdisplay_subtext%2Cgeneral_info%2Cgenre%2Ccompany_overview%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAN7gkHYqNO3BnEXoKk2nZCFOHNZC7crvAUWzmU6zZA7DdSZABm2nvz8ZAZAMPPo3ZC42ESGyQxhq2QFzGiYLA1smfSar2golM7bZAUHuABY3t80MQjSqC20xdexoJBAjOW3NswkfoVzFOe2EokDXI1Fh48T9F90AnZCZB9adaB3d0UxJSjD1CZCI6cZD'
+facebook_url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Babout%2Ccategory%2Cname%2Cdisplay_subtext%2Cgeneral_info%2Cgenre%2Ccompany_overview%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAFwfDpZAlNNmaZBv1v40EV8ZBbGhvKQEFzbZCIfZCqMz0E0MY3OYNdkV7iBZBNDTwABZAFdxVB4f77DvdbSOxZBLMvxaN9CYThrEpXQwJQf3YwRY54egDDclgRp7ZBaLtrIZAzqkwSZALYWaSZAd4OanliwuHIMsRZCZBTiDzbXjgmy7i6lsuzW89u2wcZD'
 
 not_enough_info_string = "I don't have enough info to suggest anything for {0}. Perhaps you should ask {0} yourself."
 
@@ -44,14 +44,32 @@ def get_facebook_info(friend_name):
 def parse_likes(likes_list):
     if not likes_list:
         return None
-    else:
-        return None
+    sports_team = []
+    book = []
+    musician = []
+    clothing = []
+    computers = []
+    for like in likes_list:
+        category = like['category'].lower()
+        if category == 'sports team':
+            sports_team.append({'sports_team': like['name']})
+        elif category == 'book':
+            book.append({'book': like['name']})
+        elif category.find('musician') != -1:
+            musician.append({'musician': like['name']})
+        elif category.find('computers') != -1:
+            computers.append({'computers': like['name']})
+        elif category.find('clothing') != -1:
+            clothing.append({'clothing': like['name']})
+
+    print(sports_team + book + musician + clothing + computers)
+
 
 def get_ebay_info(items_list):
     if not items_list:
         return None
     else:
-        return None # [{1: 10}, {2: 20}, {3: 30}]
+        return None  # [{1: 10}, {2: 20}, {3: 30}]
 
 
 def get_nice_message(gifts_dict):
@@ -59,6 +77,7 @@ def get_nice_message(gifts_dict):
         return not_enough_info_string
     else:
         return None
+
 
 if __name__ == '__main__':
     app.run()
