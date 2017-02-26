1aa68b3f0b0898e241d65450b0d3a36d8f5c628a
Mark Philip <philip.mark123@gmail.com>
2017-02-26 07:45:10 -0500

    improved flow

--- backend/backend.py
+++ backend/backend.py
@@ -11,6 +11,8 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 facebook_url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Babout%2Ccategory%2Cname%2Cdisplay_subtext%2Cgeneral_info%2Cgenre%2Ccompany_overview%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAN7gkHYqNO3BnEXoKk2nZCFOHNZC7crvAUWzmU6zZA7DdSZABm2nvz8ZAZAMPPo3ZC42ESGyQxhq2QFzGiYLA1smfSar2golM7bZAUHuABY3t80MQjSqC20xdexoJBAjOW3NswkfoVzFOe2EokDXI1Fh48T9F90AnZCZB9adaB3d0UxJSjD1CZCI6cZD'
 
+not_enough_info_string = "I don't have enough info to suggest anything for {0}. Perhaps you should ask {0} yourself."
+
 
 @ask.intent("GetGiftSuggestion")
 def get_gifts(friend_name):
@@ -19,27 +21,44 @@ def get_gifts(friend_name):
     items_list = get_facebook_info(friend_name)
 
     # use facebook info to find ebay listings
-    gifts_dict = get_ebay_info(items_list[0], items_list[1], items_list[2])
+    gifts_dict = get_ebay_info(items_list)
 
-    ret_string = get_nice_message(gifts_dict[0], gifts_dict[1], gifts_dict[2]).format(friend_name)
+    ret_string = get_nice_message(gifts_dict).format(friend_name)
     return statement(ret_string).simple_card('Gift Suggestion', ret_string)
 
 
 def get_facebook_info(friend_name):
+    if not friend_name:
+        return None
     info = requests.get(facebook_url).json()
-    #for person in info['friends']['data']:
+    people = info['friends']['data']  # list of people
+    for person in people:
+        if (person['name'])[:person['name'].find(' ')].lower() == friend_name.lower():
+            print('Found {0}'.format(friend_name))
+            likes_list = person['likes']['data']
+            top_3_likes = parse_likes(likes_list)
+            return top_3_likes
+    return None
 
 
-    return [1, 2, 3]
+def parse_likes(likes_list):
+    if not likes_list:
+        return None
+    else:
+        return None
+
+def get_ebay_info(items_list):
+    if not items_list:
+        return None
+    else:
+        return None # [{1: 10}, {2: 20}, {3: 30}]
 
 
-def get_ebay_info(item1, item2, item3):
-    return [{1: 10}, {2: 20}, {3: 30}]
-
-
-def get_nice_message(gift1, gift2, gift3):
-    return 'I have no idea what you should get. Perhaps you should ask {0} yourself.'
-
+def get_nice_message(gifts_dict):
+    if not gifts_dict:
+        return not_enough_info_string
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run()
