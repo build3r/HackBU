c09ce6e3e4613f5363c8984d0339c18cf4921861
Mark Philip <philip.mark123@gmail.com>
2017-02-26 11:15:58 -0500

    working except for college info

--- api_test.py
+++ api_test.py
@@ -17,9 +17,19 @@ class Api(object):
         # print(r.status_code)
         if r.status_code == requests.codes.ok:
             response = r.json()
-            #for person in response['friends']['data']:
-            #    print(person['name'])
-            print(json.dumps(response, sort_keys=True, indent=4))
+            #response = response['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][0]['sellingStatus'][0]['convertedCurrentPrice'][0]['__value__']
+            for person in response['friends']['data']:
+                print(person['name'])
+                try:
+                    edu = person['education']
+                    #print(json.dumps(edu, sort_keys=True, indent=4))
+                    for school in edu:
+                        #print(json.dumps(school, sort_keys=True, indent=4))
+                        if school['type'] == 'College':
+                            print(school['school']['name'])
+                except KeyError:
+                    pass
+            #print(json.dumps(response, sort_keys=True, indent=4))
 
             # extract the name and current selling price from Json for the top item
         else:
@@ -28,8 +38,8 @@ class Api(object):
 
 if __name__ == '__main__':
     # ebay
-    # url = 'http://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=RobinLi-HackBU-SBX-16c385072-25a053d6&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=Dell%20Laptops&itemFilter.paramName=Currency&itemFilter.paramValue=USD&itemFilter.value=true&paginationInput.entriesPerPage=1'
+    # url = 'http://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=RobinLi-HackBU-SBX-16c385072-25a053d6&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=sports%20team%20Cleveland%20Cavaliers&itemFilter.paramName=Currency&itemFilter.paramValue=USD&itemFilter.value=true&paginationInput.entriesPerPage=1'
     # facebook
-    url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Bcategory%2Cname%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAFwfDpZAlNNmaZBv1v40EV8ZBbGhvKQEFzbZCIfZCqMz0E0MY3OYNdkV7iBZBNDTwABZAFdxVB4f77DvdbSOxZBLMvxaN9CYThrEpXQwJQf3YwRY54egDDclgRp7ZBaLtrIZAzqkwSZALYWaSZAd4OanliwuHIMsRZCZBTiDzbXjgmy7i6lsuzW89u2wcZD'
+    url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Bcategory%2Cname%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAAZAlCuCcgKxIovBglwcZCB2VCJkB10mMAaHXTraOeE4Y2NQFIEDnmKAmEEF3TfzijZB1zVZCFvypmwd7gk1JZCpsi9OguZB8gBZAPTICVSBwGb4vmFEN74yr7pfpG9HhNlGZBodkS2Va7bZAffp4hzYJfDALv8K2bqGt5OH8KZA7HjiN3Q0oG1R8ZD'
     t = Api(url)
     t.get_response()
--- backend/backend.py
+++ backend/backend.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 ask = Ask(app, '/')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-facebook_url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Bcategory%2Cname%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAFwfDpZAlNNmaZBv1v40EV8ZBbGhvKQEFzbZCIfZCqMz0E0MY3OYNdkV7iBZBNDTwABZAFdxVB4f77DvdbSOxZBLMvxaN9CYThrEpXQwJQf3YwRY54egDDclgRp7ZBaLtrIZAzqkwSZALYWaSZAd4OanliwuHIMsRZCZBTiDzbXjgmy7i6lsuzW89u2wcZD'
+facebook_url = 'https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cfriends%7Bname%2Ceducation%2Clikes%7Bcategory%2Cname%7D%2Cbirthday%7D&access_token=EAACEdEose0cBAAZAlCuCcgKxIovBglwcZCB2VCJkB10mMAaHXTraOeE4Y2NQFIEDnmKAmEEF3TfzijZB1zVZCFvypmwd7gk1JZCpsi9OguZB8gBZAPTICVSBwGb4vmFEN74yr7pfpG9HhNlGZBodkS2Va7bZAffp4hzYJfDALv8K2bqGt5OH8KZA7HjiN3Q0oG1R8ZD'
 ebay_url = 'http://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=RobinLi-HackBU-SBX-16c385072-25a053d6&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords={0}&itemFilter.paramName=Currency&itemFilter.paramValue=USD&itemFilter.value=true&paginationInput.entriesPerPage=1'
 
 not_enough_info_string = "I don't have enough info to suggest anything for {0}. Perhaps you should ask {0} yourself."
@@ -17,6 +17,7 @@ not_enough_info_string = "I don't have enough info to suggest anything for {0}. 
 
 @ask.intent("GetGiftSuggestion")
 def get_gifts(friend_name):
+    #try:
     print('Name: {0}'.format(friend_name))
     # get info from facebook
     items_list = get_facebook_info(friend_name)
@@ -25,7 +26,23 @@ def get_gifts(friend_name):
     gifts_list = get_ebay_info(items_list)
 
     ret_string = get_nice_message(gifts_list).format(friend_name)
-    return statement(ret_string).simple_card('Gift Suggestion', ret_string)
+    return statement(ret_string).simple_card('Gift Suggester', ret_string)
+    #except:
+    #    conn_err_message = 'There seems to have been a connection error. Please try again later.'
+    #    return statement(conn_err_message).simple_card('Gift Suggester', conn_err_message)
+
+
+def get_college_info(person):
+    try:
+        edu = person['education']
+        for school in edu:
+            if school['type'] == 'College':
+                school_name = school['school']['name']
+                print(school_name)
+                return ['college', str(school_name)]
+    except KeyError:
+        pass
+    return []
 
 
 def get_facebook_info(friend_name):
@@ -37,7 +54,11 @@ def get_facebook_info(friend_name):
         if (person['name'])[:person['name'].find(' ')].lower() == friend_name.lower():
             print('Found {0}'.format(friend_name))
             likes_list = person['likes']['data']
-            top_3_likes = parse_likes(likes_list)
+            top_3_likes = parse_likes(likes_list)#.append(get_college_info(person))
+            if not top_3_likes:
+                top_3_likes = get_college_info(person)
+            else:
+                top_3_likes = top_3_likes.append(get_college_info(person))
             return top_3_likes
     return None
 
@@ -63,8 +84,9 @@ def parse_likes(likes_list):
         elif category.find('clothing') != -1:
             clothing.append(['clothing', like['name']])
 
-    print(sports_team + book + musician + clothing + computers)
     likes = sports_team + book + musician + clothing + computers
+    print(likes)
+
     i = 0
     retVal = []
     for element in likes:
@@ -81,14 +103,29 @@ def get_ebay_info(items_list):
     if not items_list:
         return None
     else:
+        gifts_list = []
         for item in items_list:
-            ebay_string = ebay_url.format(get_ebay_string(item))
-    return None
+            print("ITEM: " + str(item))
+            if item[0] != 'college':
+                ebay_string = ebay_url.format(get_ebay_string(item))
+                ebay_info = requests.get(ebay_string).json()
+                try:
+                    item_price = ebay_info['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][0]['sellingStatus'][0]['convertedCurrentPrice'][0]['__value__']
+                    gifts_list.append([item, str(item_price)])
+                except KeyError:
+                    print("Couldn't find an item matching {0} {1}".format(item[0], item[1]))
+    print(gifts_list)
+    return gifts_list
+
 
 def get_ebay_string(item):
     category = item[0]
     name = item[1]
-    retval = category + ' ' + name
+    retval = ''
+    if category == 'sports team':
+        retval = name
+    else:
+        retval = category + ' ' + name
     retval = retval.replace(' ', '%20')
     print(retval)
     return retval
@@ -99,7 +136,39 @@ def get_nice_message(gifts_list):
     if not gifts_list:
         return not_enough_info_string
     else:
-        return not_enough_info_string
+        final_message = ''
+        curr_spot = 0
+        for gift in gifts_list:
+            if gift[0][0] == 'sports team':
+                if curr_spot == 0:
+                    final_message += '{0} might like an item from the ' + gift[0][1] + '. I found one for ' + gift[1] + ' dollars on ebay.'
+                else:
+                    final_message += ' {0} might also like an item from the ' + gift[0][1] + '. I found one for ' + gift[1] + ' dollars on ebay.'
+            elif gift[0][0] == 'book':
+                if curr_spot == 0:
+                    final_message += '{0} might like the book ' + gift[0][1] + '. I found a copy for ' + gift[1] + ' dollars on ebay.'
+                else:
+                    final_message += ' {0} might also like the book ' + gift[0][1] + '. I found a copy for ' + gift[1] + ' dollars on ebay.'
+            elif gift[0][0] == 'musician':
+                if curr_spot == 0:
+                    final_message += '{0} might like something by ' + gift[0][1] + '. I found some merchandise for ' + gift[1] + ' dollars on ebay.'
+                else:
+                    final_message += ' {0} might also like something by ' + gift[0][1] + '. I found some merchandise for ' + gift[1] + ' dollars on ebay.'
+            elif gift[0][0] == 'computers':
+                if curr_spot != 0:
+                    final_message += ' '
+                final_message += 'It might be time for {0} to get some new tech. I found a ' + gift[0][1] + ' for ' + gift[1] + ' dollars on ebay.'
+            elif gift[0][0] == 'clothing':
+                if curr_spot != 0:
+                    final_message += ' '
+                final_message += 'It might be time for {0} to refresh his wardrobe. I found some apparel from ' + gift[0][1] + ' for ' + gift[1] + ' dollars on ebay.'
+            elif gift[0] == 'college':
+                if curr_spot != 0:
+                    final_message += ' You could also get {0} some swag from their alma matter, ' + gift[1] + '.'
+                else:
+                    final_message += 'You might want to get {0} some swag from their alma matter, ' + gift[1] + '.'
+            curr_spot += 1
+        return final_message
 
 
 if __name__ == '__main__':
